ba36c427ed10bb46f7378001f256982243597abc
Daniel Cogan <danield9tqh@me.com>
2017-09-04 17:09:59 -0700

    create 404 page component (#17)

---
+++ src/js/NotFound/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./style.scss";
+
+class NotFound extends React.Component {
+  render() {
+    return (
+      <div>{ "Sorry this page does not Exist" }</div>
+    );
+  }
+}
+
+export default NotFound;
